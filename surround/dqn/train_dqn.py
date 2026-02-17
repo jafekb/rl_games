f55296fb72af13c951f34aa064525d123f5665c0
Ben Jafek <bjafek@nuro.ai>
2026-02-16 20:29:40 -0800

    Only convert to grayscale, don't do the one game stuff

--- surround/dqn/train_dqn.py
+++ surround/dqn/train_dqn.py
@@ -252,8 +252,7 @@ class DQNTrainer:
                     observation = observation.copy()
                     video_writer.append_data(observation)
                 reward_t = torch.tensor([reward], device=self.device)
-                # Only train for 1 game, not the whole 10-point match.
-                done = terminated or truncated or abs(reward) == 1
+                done = terminated or truncated
 
                 if terminated:
                     next_state = None
