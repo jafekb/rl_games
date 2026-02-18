ef758e2d28f031ec1cab8656ea064fa09c9918fb
Ben Jafek <bjafek@nuro.ai>
2026-02-18 00:26:04 -0800

    Just some flags

--- scripts/tb.sh
+++ scripts/tb.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
+export LOG_DIR="runs/surround"
 
-uv run --isolated --with "setuptools<70" --with tensorboard tensorboard --logdir runs/surround
+uv run --isolated --with "setuptools<70" --with tensorboard tensorboard --logdir $LOG_DIR --load_fast=true
