efb8f1567a1ef0620e2b3a01d915964df522e0b6
Ben Jafek <bjafek@nuro.ai>
2026-02-24 16:00:47 -0800

    Reduce MAX_CYCLES and print out some logging info

--- surround/dqn/train_dqn.py
+++ surround/dqn/train_dqn.py
@@ -175,6 +175,7 @@ class DQNTrainer:
             raise FileExistsError(
                 f"Log dir already exists: {constants.DQN_LOG_DIR}. Remove it before a fresh run."
             )
+        print(f"Saving checkpoint and run information to {constants.DQN_LOG_DIR}")
         self._run_metadata = _get_run_metadata()
         logging.getLogger("tensorboardX").setLevel(logging.ERROR)
         self.writer = SummaryWriter(log_dir=str(constants.DQN_LOG_DIR))
